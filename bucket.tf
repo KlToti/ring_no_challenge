d1cc242a91e60de1f69153870c8da55369ed65aa
pauleentalay <pauleen.talay@cloudreach.com>
2022-10-17 16:00:37 +0200

    feat create notification s3:creation to lambda function

--- bucket.tf
+++ bucket.tf
@@ -24,22 +24,11 @@ resource "aws_s3_bucket_versioning" "version_my_bucket" {
   bucket = aws_s3_bucket.ring_bucket.id
 
   versioning_configuration {
-    status = "Enabled"
+    status = "Disabled"                                 #Disabled to allow overwriting of files, one file name
   }
 }
-resource "aws_dynamodb_table" "terraform_lock_tbl" {
-  name           = "terraform-lock"
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
 
-  tags           = {
-    Name = "terraform-lock"
-  }
-}
+
+
 
--- lambda.tf
+++ lambda.tf
@@ -18,12 +18,12 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
-resource "aws_lambda_function" "test_lambda" {
+resource "aws_lambda_function" "notification_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   filename      = "${path.module}/files/trigger.zip"
   function_name = "send_notification"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.iam_for_lambda.arn 
   #entry point of the lambda function, which function is going to be called first
   handler       = "trigger.lambda_handler" 
 
@@ -36,8 +36,29 @@ resource "aws_lambda_function" "test_lambda" {
 
 }
 
-data "archive_file" "lambda_deplyment" {
+data "archive_file" "lambda_deployment" {
   type        = "zip"
   source_file = var.path_to_triggerLambda
   output_path = var.path_to_output
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.notification_lambda.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = var.bucket_name.arn                 # change later to ARN from other user
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = var.bucket_name.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.lambda_deployment.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "AWSLogs/"
+    filter_suffix       = ".log"
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
 }
