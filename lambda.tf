7ba4af622eb72cf8b3ac241f645d80a88d1e19fd
pauleentalay <pauleen.talay@cloudreach.com>
2022-10-17 17:22:15 +0200

    fix lambda_function_arn in aws_s3_bucket_notification, fix comments

--- lambda.tf
+++ lambda.tf
@@ -44,17 +44,17 @@ data "archive_file" "lambda_deployment" {
 
 resource "aws_lambda_permission" "allow_bucket" {
   statement_id  = "AllowExecutionFromS3Bucket"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.notification_lambda.arn
+  action        = "lambda:InvokeFunction"             # trigger
+  function_name = aws_lambda_function.notification_lambda.arn       # lambda_funciton
   principal     = "s3.amazonaws.com"
-  source_arn    = var.bucket_name.arn                 # change later to ARN from other user
+  source_arn    = var.bucket_name.arn                 # own bucket
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = var.bucket_name.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.lambda_deployment.arn
+    lambda_function_arn = aws_lambda_function.notification_lambda.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "AWSLogs/"
     filter_suffix       = ".log"
