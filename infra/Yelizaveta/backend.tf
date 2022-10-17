76424d55801efee429af1fb295a84561979e11fc
KlToti <113111867+KlToti@users.noreply.github.com>
2022-10-17 13:06:02 +0200

    Delete infra directory

--- infra/Yelizaveta/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ta-terraform-tfstates-041308375526"
-    key            = "project/ring_no_challange/Team3/ec2/terraform.tfstates"
-    region = "eu-central-1"
-    dynamodb_table = "terraform-lock"
-    profile = "talent-academy"
-  }
-}
