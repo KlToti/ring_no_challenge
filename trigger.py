91954b5a7f3b4a661682f4c78bdd01ff0e8ee5ca
pauleentalay <pauleen.talay@cloudreach.com>
2022-10-18 14:40:35 +0200

    add get_public_ip, post request to server; fix bucket names with environment variables

--- trigger.py
+++ trigger.py
@@ -1,6 +1,27 @@
 import json
 import boto3
 import urllib
+import requests
+import os
+
+### environment variables
+forward_account_id = os.getenv.forward_account_id
+account_id = os.getenv.account_id
+
+#bucket names
+bucket_name = f'{account_id}-ring-no-challange-talent-academy-project-oct-2022'
+forward_bucket_name = f'{account_id}-ring-no-challange-talent-academy-project-oct-2022'
+
+#get public ip address from my account with tag name
+def get_public_ip():
+    ec2 = boto3.resource('ec2')
+    running_instances = ec2.instances.filter(Filters=[
+        {'Name': 'instance-state-name',
+        'Values': ['running']},
+        {'Name': "tag:Name", "Values": ["Ring_no_challange_webserver"]}])
+    public_ips = [instance.public_ip_address for instance in running_instances]
+    public_ip = public_ips[0]
+    return public_ip
 
 def lambda_handler(event,context):
     s3_client = boto3.client('s3')
@@ -20,7 +41,6 @@ def lambda_handler(event,context):
     value = data['number']
 
     #integrate the fuction by incrementing it by one 
-
     new_value = value +1
     new_data = {'number': int(new_value), 'account_id' : account }
     with open(content, "w") as jsonFile:
@@ -29,13 +49,24 @@ def lambda_handler(event,context):
     #put the updated file in the bucket
     # TO-DO:define the target bucket 
     #next_s3 = boto3.resource('s3') 
-    next_s3 = boto3.resource (
-        service_name = 's3',
-        region_name = var.region,
-        aws_access_key_id = var.aws_access_key_id,
-        aws_secret_access_key = var.aws_secret_access_key
     
-    )
-    response = next_s3.put_object(Body = body, Key = content)
+    # next_s3 = boto3.resource (
+    #     service_name = 's3',
+    #     region_name = var.region,
+    #     aws_access_key_id = var.aws_access_key_id,
+    #     aws_secret_access_key = var.aws_secret_access_key
+    
+    #send file to my webserver
+    public_ip = str(get_public_ip())
+    resp = requests.post(public_ip, json=new_data)
+    print(resp) #response from web_server
+    
+    #keep as environment variable, just in case
+    os.environ["MY_RING_WEB_SERVER"] = public_ip
+    
+    #send to the next bucket using you as a user
+    response = s3_client.put_object(Body=body, Bucket=forward_bucket_name, Key = content)
+    
+
     #response = client_s3.put_object(Body = body, Bucket = var.next_bucket, Key = content)
 
