9cb6b7cf85b980d102d1cbd5c4378b977aed5b2f
pauleentalay <pauleen.talay@cloudreach.com>
2022-10-20 07:30:22 +0200

    fix s3 notification

--- lambda.tf
+++ lambda.tf
@@ -93,8 +93,6 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.func.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "AWSLogs/"
-    filter_suffix       = ".log"
   }
 
   depends_on = [aws_lambda_permission.allow_bucket]
